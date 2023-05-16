00bd1e505be8de57f7462d2103a1cb1d040cba9b
kanghoon-choi <kanghoon.choi@tosspayments.com>
2023-05-16 22:50:30 +0900

    fix: client kakao, naver 빈 생성 안되는 버그 수정

---
+++ chooz-api/src/main/java/com/cdg/chooz/config/filter/ErrorCode.java
@@ -0,0 +1,23 @@
+package com.cdg.chooz.config.filter;
+
+import com.cdg.chooz.domain.common.EnumModel;
+
+public enum ErrorCode implements EnumModel {
+    TokenExpired("401");
+
+    private String value;
+
+    ErrorCode(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String getKey() {
+        return name();
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+}
